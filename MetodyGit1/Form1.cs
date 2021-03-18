cb27b66da714a00b1286b679ed8fbdf21837741f
Jakub Severin <severinj.03@in.spst.cz>
2021-03-18 13:38:19 +0100

    Add project files.

---
+++ MetodyGit1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetodyGit1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private DateTime nejstarsi(DateTime prvni, DateTime druhy, DateTime treti)
+        {
+            TimeSpan prvniVek = DateTime.Today - prvni;
+            TimeSpan druhyVek = DateTime.Today - druhy;
+            TimeSpan tretiVek = DateTime.Today - treti;
+            if(prvniVek > druhyVek && prvniVek > tretiVek)
+            {
+                return prvni;
+            }
+            else if(druhyVek > prvniVek && druhyVek > tretiVek)
+            {
+                return druhy;
+            }
+            else
+            {
+                return treti;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime prvni = DateTime.Parse(textBox1.Text);
+            DateTime druhy = DateTime.Parse(textBox2.Text);
+            DateTime treti = DateTime.Parse(textBox3.Text);
+            MessageBox.Show("Nejstarsi datum je " + nejstarsi(prvni, druhy, treti).ToShortDateString());
+        }
+    }
+}
