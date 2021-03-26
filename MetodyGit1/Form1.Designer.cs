8a82f06c98cd6491e01cca2cdb750dc1dfb9c1db
JiriPokorny29 <tyrex48@seznam.cz>
2021-03-26 16:38:18 +0100

    Metody1

--- MetodyGit1/Form1.Designer.cs
+++ MetodyGit1/Form1.Designer.cs
@@ -32,51 +32,68 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(255, 104);
+            this.button1.Location = new System.Drawing.Point(191, 84);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(72, 19);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Nejstarsi";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(149, 72);
+            this.textBox1.Location = new System.Drawing.Point(112, 58);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "27.1.2003";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(149, 105);
+            this.textBox2.Location = new System.Drawing.Point(112, 85);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.Size = new System.Drawing.Size(76, 20);
             this.textBox2.TabIndex = 2;
             this.textBox2.Text = "8.6.2003";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(149, 133);
+            this.textBox3.Location = new System.Drawing.Point(112, 108);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.Size = new System.Drawing.Size(76, 20);
             this.textBox3.TabIndex = 3;
             this.textBox3.Text = "14.12.2000";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(191, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 20);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Nejmladsi";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -90,6 +107,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- MetodyGit1/Form1.cs
+++ MetodyGit1/Form1.cs
@@ -35,13 +35,40 @@ namespace MetodyGit1
                 return treti;
             }
         }
-
+        //Metoda Nejstarsi funguje spravne
+        private DateTime nejmladsi(DateTime prvni, DateTime druhy, DateTime treti)
+        {
+            TimeSpan prvniVek = DateTime.Today - prvni;
+            TimeSpan druhyVek = DateTime.Today - druhy;
+            TimeSpan tretiVek = DateTime.Today - treti;
+            if (prvniVek < druhyVek && prvniVek < tretiVek)
+            {
+                return prvni;
+            }
+            else if (druhyVek < prvniVek && druhyVek < tretiVek)
+            {
+                return druhy;
+            }
+            else
+            {
+                return treti;
+            }
+        }
+        //Metoda nejmladsi vyhleda nejmladsiho cloveka a vypise do messageboxu
         private void button1_Click(object sender, EventArgs e)
         {
             DateTime prvni = DateTime.Parse(textBox1.Text);
             DateTime druhy = DateTime.Parse(textBox2.Text);
             DateTime treti = DateTime.Parse(textBox3.Text);
-            MessageBox.Show("Nejstarsi datum je " + nejstarsi(prvni, druhy, treti).ToShortDateString());
+            MessageBox.Show("Nejstarsi datum je " + nejstarsi(prvni, druhy, treti).ToShortDateString()); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime prvni = DateTime.Parse(textBox1.Text);
+            DateTime druhy = DateTime.Parse(textBox2.Text);
+            DateTime treti = DateTime.Parse(textBox3.Text);
+            MessageBox.Show("Nejmladsi datum je " + nejmladsi(prvni, druhy, treti).ToShortDateString());
         }
     }
 }
